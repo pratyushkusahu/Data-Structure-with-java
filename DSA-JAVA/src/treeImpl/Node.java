60bc721a7385c1c1be5fcd067542f1d13d5ee95e
Pratyush <pratyush.sahu@tejosma.com>
2023-08-03 16:55:46 +0530

    Binary Tree Implimaintation

---
+++ DSA-JAVA/src/treeImpl/Node.java
@@ -0,0 +1,11 @@
+package treeImpl;
+
+public class Node {
+	int value;
+	Node left, right;
+
+	public Node(int item) {
+		value = item;
+		left = right = null;
+	}
+}
---
+++ DSA-JAVA/src/treeImpl/Tree.java
@@ -0,0 +1,85 @@
+package treeImpl;
+
+import DSA.treeImpl.Node;
+
+public class Tree { 
+	//    static class Node {    
+	//    int value; 
+	//        Node left, right; 
+	//          
+	//        Node(int value){ 
+	//            this.value = value; 
+	//            left = null; 
+	//            right = null; 
+	//        } 
+	//    } 
+
+	public void insert(Node node, int value) {
+		//left child Insertion 
+		if (value < node.value) { 
+			if (node.left != null)
+			{
+				insert(node.left, value); 
+			} else { 
+				System.out.println(" Inserted " + value + " to left of " + node.value); 
+				node.left = new Node(value);
+			} 
+		} 
+		//Right child insertion 
+		else if (value > node.value) {
+			if (node.right != null) {
+				insert(node.right, value);
+			} else {
+				System.out.println(" Inserted " + value + " to right of "+ node.value);
+				node.right = new Node(value);
+			}
+		}
+	}
+	public void traverseInOrder(Node node) {
+		if (node != null) {
+			traverseInOrder(node.left);
+			System.out.print(" " + node.value);
+			traverseInOrder(node.right);
+		}
+	}
+	
+	public void preOrdertraversal(Node root) {
+		if(root!=null) {
+			System.out.print(root.value+ "  ");
+			preOrdertraversal(root.left);
+			preOrdertraversal(root.right);
+		}
+	}
+	
+	public void postOrdertraversal(Node root) {
+		if(root!=null) {
+			postOrdertraversal(root.right);
+			postOrdertraversal(root.left);
+			System.out.print(root.value+"  ");
+		}
+	}
+
+
+	public static void main(String args[]) 
+	{ 
+		Tree tree = new Tree();
+		Node root = new Node(5);
+		System.out.println("Binary Tree Example");
+		System.out.println("Building tree with root value " + root.value);
+		tree.insert(root, 2);
+		tree.insert(root, 4);
+		tree.insert(root, 8);
+		tree.insert(root, 6);
+		tree.insert(root, 7);
+		tree.insert(root, 3);
+		tree.insert(root, 9);
+		tree.insert(root, 50);
+		System.out.println("\nIn Order Traversal of tree");
+		tree.traverseInOrder(root);
+		System.out.println("\nPre- Order Traversal of tree");
+		tree.preOrdertraversal(root);
+		System.out.println("\nPost-Order Traversal of tree");
+		tree.postOrdertraversal(root);
+	}
+}
+
